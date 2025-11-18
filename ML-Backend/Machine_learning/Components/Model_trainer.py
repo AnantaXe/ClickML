8433ef1b1bb2cb6bef9c2c6d5b49e7d02b4685cc
raghavbankar <raghavendrabankar@gmail.com>
2025-11-18 14:51:32 +0530

    FastApi Routes Updated and params handeled

--- ML-Backend/Machine_learning/Components/Model_trainer.py
+++ ML-Backend/Machine_learning/Components/Model_trainer.py
@@ -7,7 +7,7 @@ from sklearn.tree import DecisionTreeClassifier,DecisionTreeRegressor
 from Machine_learning.Components.utils import evaluate_model,fetch_data_postgresql
 from Machine_learning.Components.Data_preprocess import Preprocess
 from Machine_learning.Components.data_transformation import DataTransformation
-from Machine_learning.Components.utils import upload_to_s3
+from Machine_learning.Components.utils import upload_to_s3,params_handle
 
 @dataclass
 class ModelTrainerConfig:
@@ -18,7 +18,7 @@ class ModelTrainer:
     def __init__(self):
         self.model_trainer_config=ModelTrainerConfig
 
-    def model_object_saving_to_s3(self,train_array,test_array,model,params:dict,user_name,Model_name):
+    def model_object_saving_to_s3(self,train_array,test_array,model,params:dict,user_name,Model_name,isclassify:bool):
     
             X_train,y_train,X_test,y_test=(
                 train_array[:,:-1],
@@ -28,7 +28,7 @@ class ModelTrainer:
             )
 #evaluate models is a function in utils that returns the model and its report local path
             model_report_path,Trained_model = evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                               model=model,param=params)
+                                               model=model,param=params,isclassify=isclassify)
             
             #updating the user name in the s3 bucket
             dir_path:str= f"Users_models/{user_name}/Models/{Model_name}" 
@@ -38,7 +38,7 @@ class ModelTrainer:
             
     
 
-    def model_training_initiate(DBsource,Target_feature,model,params,user_name,model_name):
+    def model_training_initiate(DBsource,Target_feature,model,params,user_name,model_name,isclassify:bool):
         
         Data=fetch_data_postgresql(DBsource) # from configuration file
         Data=Preprocess(Data)# returns preprocessed train and test dataset
@@ -46,10 +46,11 @@ class ModelTrainer:
         Data_Transformation=DataTransformation()
         Trans_Train,Trans_Test=Data_Transformation.initiate_data_transformation(Data,Target_feature) # returns transformed Data
 
+        handled_parms=params_handle(params,model)
         model=ModelTrainer.Getting_model_object(model)
 
         ModelTrain=ModelTrainer()
-        model_path_s3,report_path_s3=ModelTrain.model_object_saving_to_s3(Trans_Train,Trans_Test,model,params,user_name,model_name)
+        model_path_s3,report_path_s3=ModelTrain.model_object_saving_to_s3(Trans_Train,Trans_Test,model,handled_parms,user_name,model_name,isclassify=isclassify)
         return model_path_s3,report_path_s3
         
         
--- ML-Backend/main.py
+++ ML-Backend/main.py
@@ -9,23 +9,121 @@ app = FastAPI()
 #     DBsource:dict # Data source dict format
 #     modelConfig:dict
      
-@app.post("/model_training")
+# @app.post("/model_training")
+# async def create_item(request: Request):
+#     try:
+#         req = await request.json()
+#         user_name = req.get("user_name")
+
+#         DBsource = req.get("DBsource", {})
+#         target_feature = DBsource.get("features", {}).get("targetf")
+#         modelConfig = req.get("modelConfig", {})
+#         model_type = modelConfig.get("modelType")
+#         params = modelConfig.get("modelparams")
+#         model_name = modelConfig.get("modelName")
+    
+#         model_path_s3, report_path_s3 = ModelTrainer.model_training_initiate(DBsource, target_feature, model_type, params, user_name, model_name)
+
+#     except Exception as e:
+#         return  {"status":"failure","message":str(e)}
+    
+#     return {"status": "success", "model_path_s3": model_path_s3, "report_path_s3": report_path_s3}
+
+#separte route for all the models
+@app.post("/linear_regression")
+async def create_item(request: Request):
+    try:
+        req:dict= await request.json()
+        user_name = "Trainer"
+
+        DBsource: dict= req.get("DBSource",{})
+        target_feature = DBsource.get("features",{}).get("targetf")
+        modelConfig:dict = req.get("modelConfig",{})
+        # model_type = modelConfig.get("modelType")
+        params = modelConfig.get("modelParams",{})
+        model_name = modelConfig.get("modelName")
+    
+        model_path_s3, report_path_s3 = ModelTrainer.model_training_initiate(DBsource, target_feature, "Linear_regression", params, user_name, model_name,False)
+    
+        return {"status": "success", "model_path_s3": model_path_s3, "report_path_s3": report_path_s3}
+    except Exception as e:
+        return  {"status":"failure","message":str(e)}
+
+@app.post("/decision_tree_regressor")
 async def create_item(request: Request):
     try:
         req = await request.json()
-        user_name = req.get("user_name")
+        user_name = "Trainer2"
 
-        DBsource = req.get("DBsource", {})
+        DBsource = req.get("DBSource", {})
         target_feature = DBsource.get("features", {}).get("targetf")
         modelConfig = req.get("modelConfig", {})
         model_type = modelConfig.get("modelType")
-        params = modelConfig.get("modelparams")
+        params = modelConfig.get("modelParams")
         model_name = modelConfig.get("modelName")
     
-        model_path_s3, report_path_s3 = ModelTrainer.model_training_initiate(DBsource, target_feature, model_type, params, user_name, model_name)
+        model_path_s3, report_path_s3 = ModelTrainer.model_training_initiate(DBsource, target_feature, "Decision_tree_Regression", params, user_name, model_name,False)
 
     except Exception as e:
         return  {"status":"failure","message":str(e)}
     
     return {"status": "success", "model_path_s3": model_path_s3, "report_path_s3": report_path_s3}
 
+@app.post("/random_forest_regressor")
+async def create_item(request: Request):
+    try:
+        req = await request.json()
+        user_name = "Trainer3"
+
+        DBsource = req.get("DBSource", {})
+        target_feature = DBsource.get("features", {}).get("targetf")
+        modelConfig = req.get("modelConfig", {})
+        model_type = modelConfig.get("modelType")
+        params = modelConfig.get("modelParams", {})
+        model_name = modelConfig.get("modelName")
+    
+        model_path_s3, report_path_s3 = ModelTrainer.model_training_initiate(DBsource, target_feature, "Random_forest_Regression", params, user_name, model_name,False)
+
+    except Exception as e:
+        return  {"status":"failure","message":str(e)}
+    
+    return {"status": "success", "model_path_s3": model_path_s3, "report_path_s3": report_path_s3}
+
+@app.post("/decision_tree_classifier")
+async def create_item(request: Request):
+    try:
+        req = await request.json()
+        user_name="regghdb"
+        DBsource = req.get("DBSource", {})
+        target_feature = DBsource.get("features", {}).get("targetf")
+        modelConfig = req.get("modelConfig", {})
+        model_type = modelConfig.get("modelType")
+        params = modelConfig.get("modelParams")
+        model_name = modelConfig.get("modelName")
+    
+        model_path_s3, report_path_s3 = ModelTrainer.model_training_initiate(DBsource, target_feature, "Decision_tree_Classifier", params, user_name, model_name,True)
+
+    except Exception as e:
+        return  {"status":"failure","message":str(e)}
+    
+    return {"status": "success", "model_path_s3": model_path_s3, "report_path_s3": report_path_s3}
+
+@app.post("/random_forest_classifier")
+async def create_item(request: Request):
+    try:
+        req = await request.json()
+        user_name = "Trainer2"
+
+        DBsource = req.get("DBSource", {})
+        target_feature = DBsource.get("features", {}).get("targetf")
+        modelConfig = req.get("modelConfig", {})
+        model_type = modelConfig.get("modelType")
+        params = modelConfig.get("modelParams")
+        model_name = modelConfig.get("modelName")
+    
+        model_path_s3, report_path_s3 = ModelTrainer.model_training_initiate(DBsource, target_feature, "Random_forest_Classifier", params, user_name, model_name,True)
+
+    except Exception as e:
+        return  {"status":"failure","message":str(e)}
+    
+    return {"status": "success", "model_path_s3": model_path_s3, "report_path_s3": report_path_s3}
